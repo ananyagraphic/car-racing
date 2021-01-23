33d580e6f1c4911d0387fd91a6f0433f80ece01c
ananyagraphic <75197504+ananyagraphic@users.noreply.github.com>
2021-01-23 18:30:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var ball;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,50,50);
+    ball.shapeColor = "pink";
+}
+
+function draw(){
+    background("yellow");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
